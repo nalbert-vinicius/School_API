11b0799435a133b3b51d0d6f96085384add54f70
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-01-04 20:54:20 -0300

    Iniciando testes de rotas e controllers com .net

---
+++ School.API/Controllers/AlunosController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace School.API.Controllers
+{
+    [Route("api/aluno")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        public AlunosController() { }
+
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno(){
+                Id = 1,
+                Nome = "Nalbertinho",
+                Sobrenome = "Vinicius"
+            },
+            new Aluno(){
+                Id = 2,
+                Nome = "Cloe",
+                Sobrenome = "Nilda"
+            },
+            new Aluno(){
+                Id = 3,
+                Nome = "Gaby",
+                Sobrenome = "Leonel"
+            },
+            new Aluno(){
+                Id = 4,
+                Nome = "Teste",
+                Sobrenome = "Vinicius"
+            },
+
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("testeRota/{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            var alunos = Alunos.FirstOrDefault(x => x.Id == Id);
+            if(alunos == null)
+            {
+                return BadRequest("Aluno não foi encontrado");
+            }
+
+            return Ok(alunos);
+        }
+    }
+}
--- School.API/Models/Professor.cs
+++ School.API/Models/Professor.cs
@@ -7,7 +7,16 @@ namespace School.API.Models
 {
     public class Professor
     {
+        public Professor(){ }
 
+        public Professor(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IEnumerable<Disciplina> Disciplina { get; set; }
     }
 }
