fc6bf054f064677e20adeaac7831037de8869236
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-01-03 21:01:04 -0300

    Iniciando a models de professores, alunos e disciplinas

---
+++ README.txt
@@ -0,0 +1,8 @@
+Projeto API REST de uma escola
+
+ - Controle de Alunos
+ - Controle de Cursos
+ - Controle de Professores
+ - Relacionamento entre as entidades
+
+
---
+++ School.API/Models/Professor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.API.Models
+{
+    public class Professor
+    {
+
+
+    }
+}
