8b911db2ae8ae336bd435ff199f2fe232de2a849
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-01-06 22:06:27 -0300

    iniciando SQLite migrations manipulação de dados via postman

--- School.API/Controllers/AlunosController.cs
+++ School.API/Controllers/AlunosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School.API.Data;
 using School.API.Models;
 using System;
 using System.Collections.Generic;
@@ -12,37 +14,18 @@ namespace School.API.Controllers
     [ApiController]
     public class AlunosController : ControllerBase
     {
-        public AlunosController() { }
-
-        public List<Aluno> Alunos = new List<Aluno>()
+        private readonly DataContext _context;
+        public AlunosController(DataContext context) 
         {
-            new Aluno(){
-                Id = 1,
-                Nome = "Nalbertinho",
-                Sobrenome = "Vinicius"
-            },
-            new Aluno(){
-                Id = 2,
-                Nome = "Cloe",
-                Sobrenome = "Nilda"
-            },
-            new Aluno(){
-                Id = 3,
-                Nome = "Gaby",
-                Sobrenome = "Leonel"
-            },
-            new Aluno(){
-                Id = 4,
-                Nome = "Teste",
-                Sobrenome = "Vinicius"
-            },
+            _context = context;
+        }
 
-        };
+ 
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(Alunos);
+            return Ok(_context.Alunos);
         }
 
         //Da para realizar tipagem do parametro na url /{id:int}
@@ -50,7 +33,7 @@ namespace School.API.Controllers
         [HttpGet("testeRota/{Id}")]
         public IActionResult GetById(int Id)
         {
-            var alunos = Alunos.FirstOrDefault(x => x.Id == Id);
+            var alunos = _context.Alunos.FirstOrDefault(x => x.Id == Id);
             if(alunos == null)
             {
                 return BadRequest("Aluno não foi encontrado");
@@ -62,18 +45,29 @@ namespace School.API.Controllers
         [HttpPost]
         public IActionResult Post(Aluno aluno)
         {
+            _context.Add(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPut("{Id}")]
-        public IActionResult Put(int Id)
+        public IActionResult Put(int Id, Aluno aluno)
         {
+            //asNotracking não bloqueia o registro no banco
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(x => x.Id == Id);
+            if (alu == null){return BadRequest("Aluno não foi encontrado");}
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok("teste");
         }
 
         [HttpDelete("{Id}")]
         public IActionResult Delete(int Id)
         {
+            var alu = _context.Alunos.FirstOrDefault(x => x.Id == Id);
+            if (alu == null) { return BadRequest("Aluno não foi encontrado"); };
+            _context.Remove(Id);
+            _context.SaveChanges();
             return Ok("Deletado");
         }
     }
