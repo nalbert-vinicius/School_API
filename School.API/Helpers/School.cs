0b1f68680623ac7763565bb19acf58e238347689
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-01-14 22:00:32 -0300

    Inserindo Swagger e inicio da documentação da API

--- School.API/Helpers/School.cs
+++ School.API/Helpers/School.cs
@@ -29,7 +29,6 @@ namespace School.API.Helpers
 
             CreateMap<AlunosDTO, Aluno>().ReverseMap();
 
-
             /** 
              * Mapemaneto de professores
              * **/
--- School.API/Startup.cs
+++ School.API/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Logging;
 using School.API.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace School.API
@@ -36,6 +38,25 @@ namespace School.API
             //Adicionando repository do tipo Scoped usa a mesma instancia para todas solicitações em uma mesma requisição
             services.AddScoped<IRepository, Repository>();
 
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("schoolapi",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                         Title = "School API",
+                         Version = "1.0",
+                         TermsOfService = new Uri("http://TermoDeUso.com"),
+                         Description = "WebAPI escola",
+                         License = new Microsoft.OpenApi.Models.OpenApiLicense
+                         {
+                             Name = "School License",
+                             Url = new Uri("http://TermoDeUso.com")
+                         }
+                    });
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+                options.IncludeXmlComments(xmlCommentsFullPath);
+            });
+
             //Loop de jsons com newtonsoft
             services.AddControllers().AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
@@ -48,6 +69,10 @@ namespace School.API
             }
 
             app.UseRouting();
+            app.UseSwagger().UseSwaggerUI(opt => {
+                opt.SwaggerEndpoint("swagger/schoolapi/swagger.json", "schoolapi");
+                opt.RoutePrefix = "";
+            });
 
             app.UseAuthorization();
 
